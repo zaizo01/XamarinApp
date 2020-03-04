e865d875047457e81afe23cb1a7042fa83507a10
Admin <Admin@A19B00647.educacion.local>
2020-03-04 00:34:18 -0400

    first commit

---
+++ Example/Example/Example/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Example.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Example
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private async void home_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Home());
+        }
+    }
+}
---
+++ Example/Example/Example/Views/Home.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Example.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Home : TabbedPage
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void home_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
